5e72265d1c0072ebea4f74f051fcffc4f8ff092d
pnpdev10 <phannguy@cisco.com>
2024-04-23 11:42:22 -0700

    Update for IaC Demo

--- iac-demo.sh
+++ iac-demo.sh
@@ -4,6 +4,7 @@ ansible-playbook playbooks/site.yml \
 -i inventory/iac2/hosts \
 -vvvv
 
+
 # Site hierarchy (scale)
 ansible-playbook playbooks/site_scale.yml \
 --extra-vars '{"COUNT": 100}' \
@@ -14,14 +15,14 @@ ansible-playbook playbooks/site_scale.yml \
 
 # Credentials
 ansible-playbook playbooks/credentials.yml \
---tags create,assign \
+--tags create \
 -i inventory/iac2/hosts \
 -vvvv
 
 
 # Discovery
 ansible-playbook playbooks/discovery.yml \
---tags multi_range,cdp \
+--tags multi_range \
 -i inventory/iac2/hosts \
 -vvvv
 
@@ -37,22 +38,24 @@ ansible-playbook playbooks/templates.yml \
 # AC2. Manual steps.
 
 
-# Provision
+# Inventory
 ansible-playbook playbooks/inventory.yml \
---tags associate \
--i inventory/iac2/hosts \
--vvvv
-
-
-# PnP
-ansible-playbook playbooks/pnp.yml \
---tags claim_cat9k_stack,claim_ewlc \
+--tags provision \
 -i inventory/iac2/hosts \
 -vvvv
 
 
 # SWIM
 ansible-playbook playbooks/swim.yml \
---tags device_ewlc_ha \
+--tags device_e2e \
 -i inventory/iac2/hosts \
 -vvvv
+
+
+# PnP
+ansible-playbook playbooks/pnp.yml \
+--tags claim_cat9k \
+-i inventory/iac2/hosts \
+-vvvv
+
+
